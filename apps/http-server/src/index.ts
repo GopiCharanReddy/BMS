00f05ecfe6098a6c1baf8690adf3ce7bb6870976
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-02-24 22:39:02 +0530

    http-server

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,13 +1,30 @@
 import express from "express";
-
+import { db } from "@repo/db/client"
+import { users } from "@repo/db/schema"
 const app = express();
 
-const port = 3000;
+const port = 3001;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("Hello.");
 })
 
+app.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+  const data = await db.insert(users).values({
+    name, email, password
+  });
+  if (!data) {
+    return res.status(400).json({
+      error: "Error while pushing data to database."
+    })
+  }
+  return res.status(200).json({
+    message: "Signup successful."
+  })
+})
 app.listen(port, () => {
   console.log("Server is listening on port: ", port);
 });
