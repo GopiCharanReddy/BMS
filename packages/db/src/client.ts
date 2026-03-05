033476469a3cd8375f078fbe6086d2961cce7c97
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-03-05 23:31:22 +0530

    fix: module resolution

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from "./schema";
+import * as schema from "./schema.ts";
 
 config({
   path: "./.env"
