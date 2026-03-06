57cea4975aa727fcf7f26b8ec602b11f3442570a
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-03-06 21:49:28 +0530

    remove .env

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,11 +1,6 @@
-import { config } from "dotenv";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema.ts";
-
-config({
-  path: "./.env"
-});
 
 console.log(process.env.DATABASE_URL);
 const client = postgres(
